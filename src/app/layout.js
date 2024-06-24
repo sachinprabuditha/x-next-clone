7b69ad0a13f82a44fad78d34a2dda8f646b6ec5f
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-06-25 00:59:02 +0530

    feat: Add search functionality to News component
    The code changes include adding search functionality to the News component. This allows users to search for specific news articles. The search input field is added to the News component's layout, and the necessary state and event handling logic are implemented. This enhancement improves the usability of the News component and provides a more comprehensive user experience.

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,10 @@ export default function RootLayout({ children }) {
             <Sidebar />
           </div>
           <div>{children}</div> 
-          <div>
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <div className="sticky top-0 bg-white py-2">
+              <input type="text" placeholder="Search" className="bg-gray-100 border border-gray-200  rounded-3xl py-2 text-sm w-full px-4"/>
+            </div>
             <News/>
           </div>
         </div>
